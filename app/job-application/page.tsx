fc1680ca893750e94f1d4f3eab37d2482ef2587f
sudoaugustin <sudoaugustin@gmail.com>
2024-03-23 12:18:30 +0630

    add: job application

---
+++ app/job-application/page.tsx
@@ -0,0 +1,94 @@
+export default function Page() {
+  return (
+    <main className="relative z-10 max-w-2xl mx-auto px-5 py-10 lg:py-20">
+      <h1>
+        Hey Adam! <br /> <i>(If you are not Adam, please kindly foward this fu*king application to him🔪)</i>
+      </h1>
+      <br /> <br />
+      <p>
+        <i>Adam:</i> Who the fu*k are u?
+      </p>
+      <p>
+        <i>Augustin:</i> A 22yr old from a country in Asia u never heard of, Myanmar(Burma).
+      </p>
+      <br /> <br />
+      <p>
+        <i>Adam:</i> What, Asia??? Huge Timezone difference dude! Better luck with next job.
+      </p>
+      <p>
+        <i>Augustin:</i> I can explain. You said the important hours to overlap is 9am-1pm which is 7:30pm-11:30pm in my local time during
+        when my brain work the most. Yeah, my brain so active during night. So I can be available from 4am-1pm according to your timezone.
+      </p>
+      <br /> <br />
+      <p>
+        <i>Adam:</i> OK! I'm listening.
+      </p>
+      <p>
+        <i>Augustin:</i> About me. I got into programming when I'm 16 year old at my university. Before that, I don't know what is
+        programming or how the code are written. I don't know anything.
+        <br /> <br />
+        My first language was C++(I forgot everything about C++ now, only JS,TS is flowing through my veins). My first project was password
+        manager with C++.
+        <br /> <br />
+        At 2nd year, I learned Java(the nightmare) and Javascript. I didn't like Javascript at first, my first love was PHP. I got
+        interested in the payment system during that time, so I competed in a web competition and won Technical
+        Award(https://witaward.com/result/2019). After that I learned about payment methods and difference between cash usage in Europe and
+        Asia. I also learned Laravel Basic.
+        <br /> <br />I learned React and NodeJS during covid period. I got my first job as a frontend developer in a Singapore company
+        2years ago. At my job, I primarily work on client projects and during free office hours I and my team work on nextjs boilerplate.
+      </p>
+      <br /> <br />
+      <p>
+        <i>Adam:</i> Could you tell me about some of the projects you are proud of.
+      </p>
+      <p>
+        <i>Augustin:</i> Well, I have three.
+      </p>
+      <br />
+      <p>
+        First is "Supex". It's a cross-browser extension tool, I release this year. This is my favourite project of all time. But I failed
+        to attract developers attention. You can see about the project{' '}
+        <a href="https://supex.dev/" className="shine">
+          here
+        </a>
+        .
+      </p>
+      <br />
+      <p>
+        Second is "VSLook". A VSCode extension which let you create your own VSCode theme with TailwindCSS colors. It was the most
+        challenging project for me. It has 7K installs. You can see the project{' '}
+        <a href="https://marketplace.visualstudio.com/items?itemName=sudoaugustin.vslook" className="shine">
+          here
+        </a>
+        .
+      </p>
+      <br />
+      <p>
+        Last one is "TailwindCSS Transpiler". It was my first side project. It's a VSCode extension which transpile tailwindcss into purecss
+        on file save. This extension has 51K installs. You can see{' '}
+        <a href="https://marketplace.visualstudio.com/items?itemName=sudoaugustin.tailwindcss-transpiler" className="shine">
+          here
+        </a>
+        . I also release a Atom version of TailwindCSS Transpiler but I unpublished due to very low installs.
+      </p>
+      <br />
+      If you want to know more of my side projects please visit{' '}
+      <a href="https://www.augustin.zip/" className="underline">
+        https://www.augustin.zip/
+      </a>
+      .
+      <br /> <br />
+      <p>
+        <i>Adam:</i> Do you wanna add something in the application?
+      </p>
+      <p>
+        <i>Augustin: </i> Yes, I do have a hearing problem. I'm not deaf. But I have problem of interpreting words when the person speak
+        sudden fast.
+      </p>
+      <br /> <br />
+      <p>
+        <i>Adam:</i> OK!! You are not hired anyway due to timezone difference. Better luck with next job.
+      </p>
+    </main>
+  );
+}
